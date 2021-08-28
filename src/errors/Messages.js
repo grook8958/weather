d94b98b3d5fdfaedd448cb7bbacb41bde329d57e
grook8958 <clement.pusks@gmail.com>
2021-08-28 18:21:42 +0200

    Added new errors for the request handler

--- src/errors/Messages.js
+++ src/errors/Messages.js
@@ -6,7 +6,12 @@ const Messages = {
   INVALID_API_KEY: 'An invalid API key was provided, get your own at https://weatherapi.com/pricing.aspx',
   API_KEY_MISSING: 'Request to use API key, but no API key was provided',
 
-  INVALID_TYPE: (name, expected, an = false) => `${name} must be a${an ? 'n' : ''} ${expected}.`
+  INVALID_TYPE: (name, expected, an = false) => `${name} must be a${an ? 'n' : ''} ${expected}.`,
+
+  UNKNOWN_METHOD: 'Invalid HTTP method was provided, the valid methods are: GET, POST, PUT, DELETE, PATCH',
+  METHOD_NOT_ALLOWED: (method) => `HTTPS method "${method}" is not supported`,
+
+  UKNOWN_API_ENDPOINT: 'The specified API endpoint is not available'
 };
 
 for (const [name, message] of Object.entries(Messages)) register(name, message);
