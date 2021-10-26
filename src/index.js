be301e7207477860afe3830d2c575d356061f7f2
grook8958 <clement.puks@gmail.com>
2021-10-26 22:25:38 +0200

    Made all the imports for all the files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
-
 module.exports = {
     //Clients
     BaseWeatherClient: require('./client/BaseWeatherClient'),
     WeatherClient: require('./client/WeatherClient'),
-
     //Structures
     Current: require('./structures/Current'),
     Location: require('./structures/Location'),
     Aqi: require('./structures/Aqi'),
     Alert: require('./structures/Alert'),
     Forecast: require('./structures/Forecast'),
-
     //Utils
     Constants: require('./Utils/Constants'),
-    Util: require('./Utils/Util')
-    
+    Util: require('./Utils/Util'),
+    //REST
+    RequestHandler: require('./rest/RequestHandler'),
+    WeatherAPIError: require('./rest/WeatherAPIError'),
+    //REST\API
+    APItypes: require('./rest/api//api-types.json'),
+    BaseAPI: require('./rest/api/BaseAPI'),
+    CurrentAPI: require('./rest/api/CurrentAPI'),
+    ForecastAPI: require('./rest/api/ForecastAPI'),
+    SearchAPI: require('./rest/api/SearchAPI'),
+    TimezoneAPI: require('./rest/api/TimezoneAPI'),
+    HistoryAPI: require('./rest/api/HistoryAPI'),
+    IPAPI: require('./rest/api/IPAPI'),
+    SportsAPI: require('./rest/api/SportsAPI'),
+    AstronomyAPI: require('./rest/api/AstronomyAPI'),
+    //Errors
+    Errors: require('./errors'),
 };
--- src/index.mjs
+++ src/index.mjs
@@ -5,14 +5,28 @@ export default mod;
 //Clients
 export const BaseWeatherClient = mod.BaseWeatherClient;
 export const WeatherClient = mod.WeatherClient;
-
 //Structures
 export const Current = mod.Current;
 export const Location = mod.Location;
 export const Aqi = mod.Aqi;
 export const Alert = mod.Alert;
 export const Forecast = mod.Forecast;
-
 //Utils
 export const Constants = mod.Constants;
 export const Utils = mod.Util;
+//REST
+export const RequestHandler = mod.RequestHandler;
+export const WeatherAPIError = mod.WeatherAPIError;
+//REST\API
+export const APItypes = mod.APItypes;
+export const BaseAPI = mod.BaseAPI;
+export const CurrentAPI = mod.CurrentAPI;
+export const ForecastAPI = mod.ForecastAPI;
+export const SearchAPI = mod.SearchAPI;
+export const TimezoneAPI = mod.TimezoneAPI;
+export const HistoryAPI = mod.HistoryAPI;
+export const IPAPI = mod.IPAPI;
+export const SportsAPI = mod.SportsAPI;
+export const AstronomyAPI = mod.AstronomyAPI;
+//Errors
+export const Errors = mod.Errors;
